087821ec0abe15adc3f4412166f5b9ddef8b9deb
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-11-09 18:44:06 +0400

    !

--- js/main.js
+++ js/main.js
@@ -11,12 +11,36 @@ function displayTime(){
     clock.innerHTML = "AM"
   }
 
-  if(hrs > 12){
-    
-  }
-
   document.querySelector(".hours").innerHTML = hrs
   document.querySelector(".minutes").innerHTML = min
   document.querySelector(".seconds").innerHTML = sec
 }
 setInterval(displayTime,10)
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 5 seconds
+}
+
+const slider = document.querySelector("#img")
+
+slider.addEventListener('mouseenter', (e) => {
+  slider.style.opacity = "0"
+});
+slider.addEventListener('mouseleave', (e) => {
+  slider.style.opacity = "1"
+});
+
+
+
